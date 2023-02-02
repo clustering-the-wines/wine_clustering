efae0f02c7ed2c2f35fe2e5a32e94e703a19f2d9
Shawn.Brown <shawn.c.brown12@gmail.com>
2023-02-02 15:33:48 -0600

    Updating workbook and modeling functions

--- model.py
+++ model.py
@@ -8,6 +8,13 @@ from sklearn.cluster import KMeans
 from sklearn.preprocessing import MinMaxScaler
 from scipy.stats import pearsonr, spearmanr
 from scipy import stats
+from sklearn.model_selection import train_test_split
+
+from sklearn.tree import DecisionTreeClassifier, plot_tree
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, confusion_matrix, plot_confusion_matrix
 
 import prepare
 import acquire
@@ -67,14 +74,139 @@ def xy_subsets(train, validate, test, target):
 
 #---------------------------------------------------------
 
+def subset_df(df, stratify=None, seed=42):
+    '''
+    This function takes in a DataFrame and splits it into train, validate, test subsets for our modeling phase. Does not take in a stratify option.
+    '''
+    train, val_test = train_test_split(df, train_size=.6, random_state=seed)
+    validate, test = train_test_split(val_test, train_size=.5, random_state=seed)
+        
+    return train, validate, test
 
 #---------------------------------------------------------
 
+def make_baseline(df, baseline, col):
+    '''
+    This function is used to create a column within the dataframe to make a baseline column, and then calculate the baseline accuracy. Needs to be optimized more, but functions as is currently. Make sure to use the word 'baseline' when calling function.
+    '''
+    
+    seed = 42
+    
+    df[baseline] = df[col].value_counts().idxmax()    
+
+    base = (df[col] == df[baseline]).mean()
+    
+    print(f'Baseline Accuracy is: {base:.3}')
+    
+#---------------------------------------------------------
+
+def rf_gen(df, col):
+    
+    metrics = []
+    
+    seed = 42
+
+    for i in range(1, 20):
+        '''
+        This function will create a dataframe of Random Forest models of varying max_depths and compare the differences from the train and validate sets and return the dataframe. It requires the variable names to be: X_train, y_train, X_validate, and y_validate.
+        '''
+        train, validate, test = subset_df(df, stratify=col)
+        X_train, y_train, X_validate, y_validate, X_test, y_test = xy_subsets(train, validate, test, col)
+        
+        X_train = pd.get_dummies(X_train, columns=['wine_type'], drop_first=True)
+        X_validate = pd.get_dummies(X_validate, columns=['wine_type'], drop_first=True)
+        X_test = pd.get_dummies(X_test, columns=['wine_type'], drop_first=True)
+        
+        rf = RandomForestClassifier(max_depth=i, min_samples_leaf=3, n_estimators=200, random_state=42)
+        rf = rf.fit(X_train, y_train)
+        
+        in_sample_accuracy = rf.score(X_train, y_train)
+        out_of_sample_accuracy = rf.score(X_validate, y_validate)
+        output = {
+            "max_depth": i,
+            "train_accuracy": in_sample_accuracy,
+            "validate_accuracy": out_of_sample_accuracy
+            }
+    
+        metrics.append(output)
+    
+    df = pd.DataFrame(metrics)
+    df["difference"] = df.train_accuracy - df.validate_accuracy
+    
+    return df
 
 #---------------------------------------------------------
 
+def dectree_gen(df, col):
+    
+    metrics = []
+    
+    seed = 42
+
+    for i in range(1, 20):
+        '''
+        This function will create a dataframe of Decision Tree models of varying max_depths and compare the differences from the train and validate sets and return the dataframe. It requires the variable names to be: X_train, y_train, X_validate, and y_validate.
+        '''
+        train, validate, test = subset_df(df, stratify=col)   
+        X_train, y_train, X_validate, y_validate, X_test, y_test = xy_subsets(train, validate, test, col)
+        
+        X_train = pd.get_dummies(X_train, columns=['wine_type'], drop_first=True)
+        X_validate = pd.get_dummies(X_validate, columns=['wine_type'], drop_first=True)
+        X_test = pd.get_dummies(X_test, columns=['wine_type'], drop_first=True)
+        
+        dectree = DecisionTreeClassifier(max_depth=i, random_state=42)
+        dectree = dectree.fit(X_train, y_train)
+
+        in_sample_accuracy = dectree.score(X_train, y_train)
+        out_of_sample_accuracy = dectree.score(X_validate, y_validate)
+        output = {
+            "max_depth": i,
+            "train_accuracy": in_sample_accuracy,
+            "validate_accuracy": out_of_sample_accuracy
+            }
+    
+        metrics.append(output)
+    
+    df = pd.DataFrame(metrics)
+    df["difference"] = df.train_accuracy - df.validate_accuracy
+    
+    return df
 
 #---------------------------------------------------------
 
+def knn_gen(df, col):
+    
+    metrics = []
+
+    seed = 42
+    
+    for i in range(1, 21):
+        '''
+        This function will create a dataframe of KNN models of varying n_neighbors and compare the differences from the train and validate sets and return the dataframe. It requires the variable names to be: X_train, y_train, X_validate, and y_validate.
+        '''
+        train, validate, test = subset_df(df, stratify=col)   
+        X_train, y_train, X_validate, y_validate, X_test, y_test = xy_subsets(train, validate, test, col)
+        
+        X_train = pd.get_dummies(X_train, columns=['wine_type'], drop_first=True)
+        X_validate = pd.get_dummies(X_validate, columns=['wine_type'], drop_first=True)
+        X_test = pd.get_dummies(X_test, columns=['wine_type'], drop_first=True)
+        
+        knn = KNeighborsClassifier(n_neighbors=i, weights='uniform')
+        knn = knn.fit(X_train, y_train)
+
+        in_sample_accuracy = knn.score(X_train, y_train)
+        out_of_sample_accuracy = knn.score(X_validate, y_validate)
+        output = {
+            "max_depth": i,
+            "train_accuracy": in_sample_accuracy,
+            "validate_accuracy": out_of_sample_accuracy
+            }
+    
+        metrics.append(output)
+    
+    df = pd.DataFrame(metrics)
+    df["difference"] = df.train_accuracy - df.validate_accuracy
+    
+    return df
 
 #---------------------------------------------------------
